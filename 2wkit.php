c4971acb77743775cc63281dbb5df532e98b7869
theblackspy <163963080+thebreachspy@users.noreply.github.com>
2025-04-12 22:50:01 -0300

    Update 2wkit.php

--- 2wkit.php
+++ 2wkit.php
@@ -4,12 +4,6 @@ require 'fmgr.php';
 require 'ntfy.php';
 require 'zdata.php';
 
-
-$t =  XorEncrypt("PARAGRAFO", "CABANA", "ALLAN");
-
-echo $t . '<p>';
-
-
-echo xorDecrypt("PARAGRAFO", "CABANA", "ALLAN");
+//JUST INCLUDE THIS FILE ON YOUR FUTURE PROJECT AND USE FUNCTIONS.
 
 ?>
